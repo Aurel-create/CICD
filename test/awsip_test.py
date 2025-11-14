3f6748071cfdf3b12ad7c6c3ba4f52fcd5b6897c
Aurel <aurel.adissin@castlebee.fr>
2025-11-14 16:08:56 +0100

    testage5

--- test/awsip_test.py
+++ test/awsip_test.py
@@ -7,5 +7,6 @@ class FindTest(unittest.TestCase):
         self.assertEqual(len(src.find_ip(awsipset, "3.10.120.3")), 2)
         self.assertEquals(src.find_ip(awsipset, "3.10.120.3")[0]['ip_prefix'], "3.8.0.0/14")
         self.assertEqual(len(src.find_ip(awsipset, "1.1.1.1")), 0)
+        
     def test_find_aws_ip(self):
         self.assertEqual(len(src.find_aws_ip("3.10.120.3")), 2)
