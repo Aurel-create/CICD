ba05bc62f2d0264b54d4cd00b2dab4d9be724414
Aurel <aurel.adissin@castlebee.fr>
2025-11-14 16:03:03 +0100

    testage5

--- test/mock_test.py
+++ test/mock_test.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import patch
 from src.awsip import find_aws_ip
 
-
 class MockTest(unittest.TestCase):
     @patch('src.awsip.download_aws_ip_set')  # Make sure this path is correct
     def test_withmock(self, mock_download):
@@ -18,4 +17,3 @@ class MockTest(unittest.TestCase):
 
         mock_download.assert_called_once()
         self.assertEqual(len(result), 0)
-
